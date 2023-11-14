3c03b3de865a24467a686ef5a8e59761dfae2724
RubenRM24 <85498197+RubenRM24@users.noreply.github.com>
2023-11-14 17:05:22 -0500

    Add files via upload

---
+++ playlistplus/src/app/dashboard/SearchBar.js
@@ -0,0 +1,25 @@
+//import * as React from "react"; 
+
+import React, { useState } from 'react';
+
+const SearchBar = ({ onSearch }) => {
+  const [query, setQuery] = useState('');
+
+  const handleSearch = () => {
+    onSearch(query);
+  };
+
+  return (
+    <div className="search-bar">
+      <input
+        type="text"
+        placeholder="Search for artists..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <button onClick={handleSearch}>Search</button>
+    </div>
+  );
+};
+
+export default SearchBar;
