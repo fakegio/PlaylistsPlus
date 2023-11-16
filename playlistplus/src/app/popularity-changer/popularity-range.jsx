e5cd4b53c036594e46e71aa33f41ccf052b038e7
Jacob Stevens <jacobstevens336@gmail.com>
2023-11-16 18:12:59 -0500

    Design update to popularity changer

--- playlistplus/src/app/popularity-changer/popularity-range.jsx
+++ playlistplus/src/app/popularity-changer/popularity-range.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Slider from '@mui/material/Slider';
+import './popularity.css'
 
 const PopularityRange = () => {
   const storedMinPopularity = parseInt(localStorage.getItem('minPopularity')) || 0;
@@ -46,30 +47,43 @@ const PopularityRange = () => {
   }, [popularityRange]);
 
   return (
-    <div className="popularity-range-container">
-      <p>Min Popularity: {popularityRange[0]}</p>
-      <input
-        type="text"
-        value={minInput}
-        onChange={handleMinInputChange}
-        onBlur={handleBlur}
-      />
-      <Slider
-        min={0}
-        max={100}
-        value={popularityRange}
-        onChange={handleSliderChange}
-        style={{ width: '80%', margin: '0 auto', height: '20px' }}
-        valueLabelDisplay="auto"
-        valueLabelFormat={(value) => `${value}`}
-      />
-      <p>Max Popularity: {popularityRange[1]}</p>
-      <input
-        type="text"
-        value={maxInput}
-        onChange={handleMaxInputChange}
-        onBlur={handleBlur}
-      />
+    <div>
+      <h1 className="title">Popularity Changer</h1>
+      <div className="popularity-range-container">
+        <div className="popularity-inputs">
+          <p>Min Popularity: {popularityRange[0]}</p>
+          <input
+            type="text"
+            value={minInput}
+            onChange={handleMinInputChange}
+            onBlur={handleBlur}
+          />
+        </div>
+        <Slider
+          min={0}
+          max={100}
+          value={popularityRange}
+          onChange={handleSliderChange}
+          style={{ width: '80%', margin: '0 auto', height: '20px' }}
+          valueLabelDisplay="auto"
+          valueLabelFormat={(value) => `${value}`}
+        />
+        <div className="popularity-inputs">
+          <p>Max Popularity: {popularityRange[1]}</p>
+          <input
+            type="text"
+            value={maxInput}
+            onChange={handleMaxInputChange}
+            onBlur={handleBlur}
+          />
+        </div>
+        <button className="button" onClick={event => window.location.href='/playlist-gen'}>
+          Playlist Generator
+        </button>
+      </div>
+      <p className="note">
+        Songs in the higher numbers are not as common, so it may not give you any songs generated.
+      </p>
     </div>
   );
 };
